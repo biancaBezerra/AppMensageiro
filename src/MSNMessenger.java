e79a850d1a6707be6c02881654c7231fe22a0d5e
biancaBezerra <bianca.skills@gmail.com>
2025-02-24 13:49:54 -0300

    initial Commit

---
+++ src/MSNMessenger.java
@@ -0,0 +1,14 @@
+public class MSNMessenger {
+    public void enviarMensagem(){
+        System.out.println("Enviando mensagem");
+    }
+    public void receberMensagem(){
+        System.out.println("Recebendo mensagem");
+    }
+    public void validarConectadoInternet(){
+        System.out.println("Validando se está conectado a internet");
+    }
+    public void salvarHistoricoMensagem(){
+        System.out.println("Salvando historico da mensagem");
+    }
+}
