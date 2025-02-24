ebbf29806a1e6c72f2b0a006715ffa3df1679332
biancaBezerra <bianca.skills@gmail.com>
2025-02-24 14:53:18 -0300

    T003 - Conceito de ABSTRACAO - DEPOIS

--- src/FacebookMessenger.java
+++ src/FacebookMessenger.java
@@ -1,3 +1,11 @@
 public class FacebookMessenger extends ServicoMensagemInstantanea{
+    @Override
+    public void enviarMensagem(){
+        System.out.println("Enviando mensagem pelo FACEBOOK");
+    }
+    @Override
+    public void receberMensagem(){
+        System.out.println("Recebendo mensagem pelo FACEBOOK\n");
+    }
 
 }
--- src/ServicoMensagemInstantanea.java
+++ src/ServicoMensagemInstantanea.java
@@ -1,26 +1,7 @@
 public abstract class ServicoMensagemInstantanea {
 
+    //A Classe abstrata tem métodos abstratos e os métodos nao tem corpo, sendo encerrados por ;
     public abstract void enviarMensagem();
     public abstract void receberMensagem();
 
-    public void enviarMensagem(){
-        //primeiro conformar se esta conectado
-        validarConectadoInternet();
-
-        System.out.println("Enviando mensagem");
-
-        //depois de enviada salvar o historico
-        salvarHistoricoMensagem();
-    }
-    public void receberMensagem(){
-        System.out.println("Recebendo mensagem\n");
-    }
-
-    //metodos privados, visiveis somente na classe
-    private void validarConectadoInternet(){
-        System.out.println("Validando se está conectado a internet");
-    }
-    private void salvarHistoricoMensagem(){
-        System.out.println("Salvando historico da mensagem");
-    }
 }
