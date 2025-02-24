327bcd94b13e04b6c2abecfdec3d300f3e807bf5
biancaBezerra <bianca.skills@gmail.com>
2025-02-24 14:03:40 -0300

    T001 - Conceito de ENCAPSULAMENTO - ANTES

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        MSNMessenger msn = new MSNMessenger();
 
+        msn.validarConectadoInternet();
+        msn.enviarMensagem();
+        msn.salvarHistoricoMensagem();
+
+        msn.receberMensagem();
     }
 }
